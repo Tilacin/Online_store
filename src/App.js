1a132988d593c27530e38b70d3a73e5764562e1d
Tilacin <tilacin45@gmail.com>
2023-03-03 14:16:31 +0300

    Перенесли кроссовки на сервер и данные для карточек подгружаем с бэка

--- src/App.js
+++ src/App.js
@@ -3,31 +3,21 @@ import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import React from "react";
 
-const arr = [
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 12999,
-    imageUrl: "/img/sneakers/1.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Air Max 270",
-    price: 15600,
-    imageUrl: "/img/sneakers/2.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 8499,
-    imageUrl: "/img/sneakers/3.jpg",
-  },
-  {
-    title: "Кроссовки Puma X Aka Boku Future Rider",
-    price: 8999,
-    imageUrl: "/img/sneakers/4.jpg",
-  },
-];
-
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+React.useEffect(() => {
+//Отправляем запрос на сервер, ответ превращаем в json формат
+fetch("https://6401bfb73779a862625cf291.mockapi.io/items")
+.then((res) => {
+  return res.json();
+})//передаём его в функцию 
+.then((json) => {
+  setItems(json);
+});
+}, [])
+
+  
 
   return (
     <div className="wrapper clear">
@@ -41,14 +31,14 @@ function App() {
             <input placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
             <Card
               title={obj.title}
               price={obj.price}
               imageUrl={obj.imageUrl}
               onFavorite={() => console.log("Добавили в закладки")}
-              onPlus={() => alert("Нажали плюс")}
+              onPlus={() => console.log("Нажали плюс")}
             />
           ))}
         </div>
