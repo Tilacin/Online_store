6c2d963340afbd48e47179d86f517fd515c3f3c1
Tilacin <tilacin45@gmail.com>
2023-03-22 21:45:22 +0300

    Настройка роутера

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-//import { HashRouter as Router } from 'react-router-dom'
+//import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
 import './index.scss';
-import 'macro-css'
+import 'macro-css';
+
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+      </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from "react-router-dom";
+// //import { HashRouter as Router } from 'react-router-dom'
+// import './index.scss';
+// import 'macro-css'
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//       <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
