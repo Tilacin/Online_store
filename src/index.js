2a5a707a3f511ab3db5e7fdca82347d378626413
Tilacin <tilacin45@gmail.com>
2023-03-24 20:38:47 +0300

    Зафиксировали корзину

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-//import { BrowserRouter as Router } from 'react-router-dom';
+
 import ReactDOM from 'react-dom';
-//import { HashRouter } from "react-router-dom";
 import { HashRouter as Router } from 'react-router-dom';
 import './index.scss';
 import 'macro-css';
@@ -20,26 +19,4 @@ ReactDOM.render(
 
 
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from "react-router-dom";
-// //import { HashRouter as Router } from 'react-router-dom'
-// import './index.scss';
-// import 'macro-css'
-// import App from './App';
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//       <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
